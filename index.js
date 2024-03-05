73a9fc905f774654e1ce6f60195e3167c30d0a78
ajlittlebro <nanain.mozo3@gmail.com>
2024-03-05 01:00:25 -0600

    Portada, footer, paginación, responsividad creados e implementados. Faltan imagenes.

--- index.js
+++ index.js
@@ -11,3 +11,22 @@ submenuItems.forEach((item) => {
     submenu.classList.remove("active");
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var scrollToTopBtn = document.getElementById("scrollToTop");
+
+  window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 100) {
+      scrollToTopBtn.classList.add("show");
+    } else {
+      scrollToTopBtn.classList.remove("show");
+    }
+  });
+
+  scrollToTopBtn.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});
