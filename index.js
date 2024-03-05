90c106a09d066d77e26ce055529bd55545fc0216
ajlittlebro <nanain.mozo3@gmail.com>
2024-03-04 23:18:30 -0600

    Todas las paginas responsivas correctamente.

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const submenuItems = document.querySelectorAll('.relative');
+const submenuItems = document.querySelectorAll(".relative");
 
-submenuItems.forEach(item => {
-    const submenu = item.querySelector('.submenu');
+submenuItems.forEach((item) => {
+  const submenu = item.querySelector(".submenu");
 
-    item.addEventListener('mouseenter', () => {
-        submenu.classList.add('active');
-    });
+  item.addEventListener("mouseenter", () => {
+    submenu.classList.add("active");
+  });
 
-    item.addEventListener('mouseleave', () => {
-        submenu.classList.remove('active');
-    });
+  item.addEventListener("mouseleave", () => {
+    submenu.classList.remove("active");
+  });
 });
-
-
